23a6ee4b7d65e34dfdee74d91ced16b65efc454d
Ourou7 <nathansutcliffeccc@gmail.com>
2023-02-01 20:24:30 +0000

    added bootstrap index template

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+// Spoonacular API playground
+ingredients = ["Apple", "Pear"]
+const settings1 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" + ingredients + "number=5&ignorePantry=true&ranking=1",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": rapidKey,
+		"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings1).done(function (response1) {
+	console.log(response1);
+
+
+});
+
+var recipeImage = response1.image
+
+//Edamam API playground
+
+const settings2 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=carbonara",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": rapidKey,
+		"X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings2).done(function (response2) {
+	console.log(response2);
+});
